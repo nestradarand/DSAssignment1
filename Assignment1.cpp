8fbaa90634dccd132db520bb9ef48fe65467a1d5
nestradarand <noah.estrada456@gmail.com>
2019-09-08 13:26:23 +0000

    file reading functionality working

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -6,6 +6,25 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-  cout << "Let's see if this is working" <<endl;
+  ifstream inputStream;
+  inputStream.open("practice.txt");
+  if(!inputStream)
+  {
+    cout << "Error occurred trying to read file" << endl;
+    exit(1);
+  }
+  string x;
+  int length;
+  //this is working to print out each character from each string
+  while(inputStream >> x)
+  {
+    length = x.size();
+    for(int i = 0;i<length;++i)
+    {
+      char currentChar = x[i];
+      cout << currentChar <<endl;
+    }
+  }
+  cout << "File successfully read from" << endl;
   return 0;
 }
