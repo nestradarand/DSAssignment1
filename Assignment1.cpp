df6bcdc8a142ba916c88fb65d9ea1de27294ac15
nestradarand <noah.estrada456@gmail.com>
2019-09-09 01:23:35 +0000

    working on counting things

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -10,11 +10,29 @@ int ccount = 0;
 int tcount = 0;
 int gcount = 0;
 
+int aacount = 0;
+int account = 0;
+int atcount = 0;
+int agcount = 0;
+int cacount = 0;
+int cccount = 0;
+int ctcount = 0;
+int cgcount = 0;
+
+int totalNucleotides = 0;
+
 void countLetters(string fileName);
+int getSum(int one,int two,int three,int four);
+void countIndividual(string str);
+void countBigrams(string str);
 
 int main(int argc, char** argv)
 {
-
+  if(!argv[1])
+  {
+    cout << "Error, no file name entered." <<endl;
+    exit(1);
+  }
   countLetters(argv[1]);
   cout << "File successfully read from" << endl;
   cout<< "Total counts:" << endl;
@@ -22,6 +40,16 @@ int main(int argc, char** argv)
   cout << ccount << endl;
   cout <<tcount << endl;
   cout <<gcount << endl;
+  cout << "Sum:" << getSum(acount,ccount,tcount,gcount)<< " Nucleotides"<<endl;
+  cout << "Bigram totals:\n" << "AA:"<< aacount << endl;
+  cout << "AC:" << account << endl;
+  cout << "AT:" << atcount << endl;
+  cout << "AG:" << agcount << endl;
+  cout << "CA:"<<cacount << endl;
+  cout << "CC:" << cccount << endl;
+  cout << "CT:" << ctcount << endl;
+  cout << "CG:" << cgcount<<endl;
+
   return 0;
 }
 void countLetters(string fileName)
@@ -33,19 +61,69 @@ void countLetters(string fileName)
     cout << "Error occurred trying to read file" << endl;
     exit(1);
   }
-  char x;
+  string x;
   int length;
   while(inputStream >> x)
   {
-    if(x == 'a')
-      acount ++;
-    else if(x == 'c')
-      ccount ++;
-    else if(x == 't')
-      tcount++;
-    else if(x == 'g')
-      gcount++;
+    countIndividual(x);
+    countBigrams(x);
     cout << x<< endl;
   }
-
+  inputStream.close();
+}
+int getSum(int one,int two,int three,int four)
+{
+  int sum = one+two+three+four;
+  return sum;
+}
+void countIndividual(string str)
+{
+  for(int i=0;i<str.size();i++)
+  {
+    char x = tolower(str[i]);
+    if(x == 'a')
+    {
+      acount ++;
+      totalNucleotides ++;
+    }
+    else if(x == 'c')
+    {
+      ccount ++;
+      totalNucleotides ++;
+    }
+    else if(x == 't')
+    {
+      tcount ++;
+      totalNucleotides ++;
+    }
+    else if(x == 'g')
+    {
+      gcount ++;
+      totalNucleotides ++;
+    }
+  }
+}
+void countBigrams(string str)
+{
+  for(int i =0;i<str.size();i++)
+  {
+    char x = tolower(str[i]);
+    char y = tolower(str[i+1]);
+    if(x == 'a' && y == 'a')
+      aacount ++;
+    else if(x == 'a' && y == 'c')
+      account ++;
+    else if(x == 'a' && y == 't')
+      atcount ++;
+    else if(x == 'a' && y == 'g')
+      agcount ++;
+    else if(x == 'c' && y == 'a')
+      cacount ++;
+    else if(x == 'c' && y == 'c')
+      ccount ++;
+    else if(x == 'c' && y == 't')
+      ctcount ++;
+    else if(x == 'c' && y == 'g')
+      cgcount ++;
+  }
 }
